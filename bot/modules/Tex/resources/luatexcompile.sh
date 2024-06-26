f96d4121fdb261a7f72983a41f192f0ded09d30e
ponte-vecchio <zkogdxdkur@p.monash.edu>
2024-06-26 20:57:36 +1000

    shellcheck

--- bot/modules/Tex/resources/luatexcompile.sh
+++ bot/modules/Tex/resources/luatexcompile.sh
@@ -1,10 +1,11 @@
-cd tex/staging/$1/
+cd "tex/staging/$1/" || exit 1
 
-chmod --quiet -R o+rwx .
+# chmod --quiet -R o+rwx .
 
 # compile_start=$(date +%s.%N)
 # sudo -u $(whoami) timeout 1m pdflatex $1.tex > texput.log 2>&1
-sudo -u "$(whoami)" timeout 1m latexmk -lualatex -no-shell-escape $1.tex > texput.log 2>&1
+# sudo -u "$(whoami)" timeout 1m latexmk -lualatex -no-shell-escape "$1.tex" > texput.log 2>&1
+timeout 1m latexmk -lualatex -no-shell-escape "$1.tex" > texput_luatex.log #2>&1
 # compile_end=$(date +%s.%N)
 # compile_time=$(echo "$compile_end - $compile_start" | bc)
 # echo "compile took $compile_time secs." >> texput.log
@@ -17,12 +18,12 @@ elif [ $RET -eq 124 ];
 then
     echo "Compilation timed out!";
 else
-    grep -A 10 -m 1 "^!" $1.log;
+    grep -A 10 -m 1 "^!" "$1.log";
 fi
 
-if [ ! -f $1.pdf ];
+if [ ! -f "$1.pdf" ];
 then
-    cp ../../failed.png $1.png
+    cp "../../failed.png" "$1.png"
     exit 1
 fi
 
@@ -33,13 +34,13 @@ fi
 # -quality <value>: JPEG/MIFF/PNG compression level
 # -repage <geometry>: size and location of an image canvas
 # -trim: trim image edges
-timeout 10 convert -density 600 -quality 90 -depth 8 -trim +repage $1.pdf -colorspace sRGB PNG64:$1.png;
+timeout 10 convert -density 600 -quality 100 -depth 16 -gamma 2 -fuzz 1% -trim +repage "$1.pdf" -colorspace RGB +profile "icc" PNG64:"$1.png" >> /dev/null;
 
 # convert_end=$(date +%s.%N)
 # convert_time=$(echo "$convert_end - $convert_start" | bc)
 # echo "convert took $convert_time secs." >> texput.log
 if [ $? -eq 124 ]; then
     echo "Image processing timed out!";
-    cp ../../failed.png $1.png
+    cp "../../failed.png" "$1.png"
     exit 1
 fi
--- bot/modules/Tex/resources/texcompile.sh
+++ bot/modules/Tex/resources/texcompile.sh
@@ -1,10 +1,10 @@
-cd tex/staging/$1/
+cd "tex/staging/$1/" || exit 1
 
-chmod --quiet -R o+rwx .
+# chmod --quiet -R o+rwx .
 
 # find . ! -name "$1.tex" -type f -exec rm -f {} +
 
-sudo -u $(whoami) timeout 1m pdflatex -no-shell-escape $1.tex > texout.log 2>&1
+timeout 1m latexmk -pdf -synctex=1 -interaction=nonstopmode -file-line-error "$1.tex" > texput.log #2>&1
 
 RET=$?
 if [ $RET -eq 0 ];
@@ -14,12 +14,12 @@ elif [ $RET -eq 124 ];
 then
  echo "Compilation timed out!";
 else
-    grep -A 10 -m 1 "^!" $1.log;
+    grep -A 10 -m 1 "^!" "$1.log";
 fi
 
-if [ ! -f $1.pdf ];
+if [ ! -f "$1.pdf" ];
 then
-  cp ../../failed.png $1.png
+  cp "../../failed.png" "$1.png"
   exit 1
 fi
 
@@ -28,11 +28,11 @@ fi
 # -quality <value>: JPEG/MIFF/PNG compression level
 # -repage <geometry>: size and location of an image canvas
 # -trim: trim image edges
-timeout 10 convert -density 600 -quality 90 -depth 8 -trim +repage $1.pdf -colorspace sRGB PNG64:$1.png;
+timeout 10 convert -density 600 -quality 90 -depth 8 -gamma 2 -fuzz 1% -trim +repage "$1.pdf" -colorspace RGB +profile "icc" PNG64:"$1.png" >> /dev/null;
 
 if [ $? -eq 124 ];
 then
  echo "Image processing timed out!";
- cp ../../failed.png $1.png
+ cp "../../failed.png" "$1.png"
  exit 1
 fi

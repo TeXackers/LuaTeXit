bc9710cbd060441c66714de6f69a2067124fe611
ponte-vecchio <zkogdxdkur@p.monash.edu>
2025-01-14 13:28:55 +1100

    add additional colours from `discord.Colour`
    Signed-off-by: ponte-vecchio <zkogdxdkur@p.monash.edu>

--- bot/modules/Guild_Utils/embed_cmd.py
+++ bot/modules/Guild_Utils/embed_cmd.py
@@ -3,35 +3,40 @@ import string
 
 import discord
 from paraCH import paraCH
-import discord
-import string
-import asyncio
-
 
 cmds = paraCH()
 
 
 discordColours = {
-    "teal": discord.Colour.teal(),
-    "dark teal": discord.Colour.dark_teal(),
-    "green": discord.Colour.green(),
-    "dark green": discord.Colour.dark_green(),
-    "blue": discord.Colour.dark_purple(),
-    "dark blue": discord.Colour.dark_blue(),
-    "purple": discord.Colour.purple(),
-    "dark purple": discord.Colour.dark_purple(),
-    "magenta": discord.Colour.magenta(),
-    "dark magenta": discord.Colour.dark_magenta(),
-    "gold": discord.Colour.gold(),
-    "dark gold": discord.Colour.dark_gold(),
-    "orange": discord.Colour.orange(),
-    "dark orange": discord.Colour.dark_orange(),
-    "red": discord.Colour.red(),
-    "dark red": discord.Colour.dark_red(),
-    "lighter grey": discord.Colour.lighter_grey(),
-    "dark grey": discord.Colour.dark_grey(),
-    "light grey": discord.Colour.light_grey(),
-    "darker grey": discord.Colour.darker_grey(),
+    "blue": discord.Colour.dark_purple(), # #71368A
+    "blurple": discord.Colour.blurple(), # #5865F2
+    "brand green": discord.Colour.brand_green(), # #57F287
+    "brand red": discord.Colour.brand_red(), # #ED4245
+    "dark blue": discord.Colour.dark_blue(), # #206694
+    "dark embed": discord.Colour.dark_embed(), # #2B2D31
+    "dark gold": discord.Colour.dark_gold(), # #C27C0E
+    "dark green": discord.Colour.dark_green(), # #1F8B4C
+    "dark grey": discord.Colour.dark_grey(), # #607d8b
+    "dark magenta": discord.Colour.dark_magenta(), # #AD1457
+    "dark orange": discord.Colour.dark_orange(), # #A84300
+    "dark purple": discord.Colour.dark_purple(), # #71368A
+    "dark red": discord.Colour.dark_red(), # #992D22
+    "dark teal": discord.Colour.dark_teal(), # #11806A
+    "darker grey": discord.Colour.darker_grey(), # #546e7a
+    "fuchsia": discord.Colour.fuchsia(), # #EB459E
+    "gold": discord.Colour.gold(), # #F1C40F
+    "green": discord.Colour.green(), # #2ECC71
+    "light grey": discord.Colour.light_grey(), # #979C9F
+    "lighter grey": discord.Colour.lighter_grey(), # #95A5A6
+    "magenta": discord.Colour.magenta(), # #E91E63
+    "og blurple": discord.Colour.og_blurple(), # #7289DA
+    "orange": discord.Colour.orange(), # #E67E22
+    "pink": discord.Colour.pink(), # #EB459F
+    "purple": discord.Colour.purple(), # #9B59B6
+    "random": discord.Colour.random(),
+    "red": discord.Colour.red(), # #E74C3C
+    "teal": discord.Colour.teal(), # #1ABC9C
+    "yellow": discord.Colour.yellow(), # #FEE75C
 }
 
 
